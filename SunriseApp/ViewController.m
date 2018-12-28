7bfdcd39abc868578fa872f6e3c7b162779aa305
Carla Urbano <carlaurbano@gmail.com>
2018-12-28 10:11:10 +0100

    Initial Commit

---
+++ SunriseApp/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  SunriseApp
+//
+//  Created by Carla Urbano on 28/12/2018.
+//  Copyright © 2018 SUNRISE. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ SunriseAppTests/SunriseAppTests.m
@@ -0,0 +1,37 @@
+//
+//  SunriseAppTests.m
+//  SunriseAppTests
+//
+//  Created by Carla Urbano on 28/12/2018.
+//  Copyright © 2018 SUNRISE. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SunriseAppTests : XCTestCase
+
+@end
+
+@implementation SunriseAppTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
